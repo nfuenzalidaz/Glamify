4454a24c76c20f158eb9dad52ecf5d93067f76db
FabioBlnco2023 <fabblasil@hotmail.com>
2023-09-14 07:26:42 -0500

    trabajando el Search

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import Login from '@/componentes/Login';
+import Search from '@/componentes/Search'; 
+
 const Page = () => {
   return (
     <div>
       <h1>Bienvenido a Glamify Mi Tienda de Ropa</h1>
       <Login />
-          
+      <Search /> 
     </div>
   )
 }
