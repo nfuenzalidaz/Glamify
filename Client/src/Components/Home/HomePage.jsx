9165ebfb135b4339ed0cb2af28bceb773849dec6
EdissonFacundo <aquinoedissonfacundo@gmail.com>
2023-09-18 02:42:51 -0300

    prettier

--- Client/src/Components/Home/HomePage.jsx
+++ Client/src/Components/Home/HomePage.jsx
@@ -1,35 +1,35 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import Pagination from "../Pagination/Pagination";
-import CardList from "../CardList/CardList";
-import Searchbar from "../searchbar/searchbar";
-import NavBar from "../NavBar/NavBar";
-import styles from "./HomePage.module.css";
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import Pagination from '../Pagination/Pagination';
+import CardList from '../CardList/CardList';
+import Searchbar from '../searchbar/searchbar';
+import NavBar from '../NavBar/NavBar';
+import styles from './HomePage.module.css';
 const Home = () => {
-  const allProducts = useSelector((state) => state.product.allProducts);
-  const itemsPerPage = 6; // Número de elementos por página
-  const [currentPage, setCurrentPage] = useState(1); // Página actual
+	const allProducts = useSelector((state) => state.product.allProducts);
+	const itemsPerPage = 6; // Número de elementos por página
+	const [currentPage, setCurrentPage] = useState(1); // Página actual
 
-  const handlePageChange = (page) => {
-    setCurrentPage(page);
-  };
+	const handlePageChange = (page) => {
+		setCurrentPage(page);
+	};
 
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentProducts = allProducts.slice(indexOfFirstItem, indexOfLastItem);
+	const indexOfLastItem = currentPage * itemsPerPage;
+	const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+	const currentProducts = allProducts.slice(indexOfFirstItem, indexOfLastItem);
 
-  return (
-    <div className={styles.container}>
-      <NavBar />
-      <Searchbar />
-      <CardList allProducts={currentProducts} />
-      <Pagination
-        totalPages={Math.ceil(allProducts.length / itemsPerPage)}
-        currentPage={currentPage}
-        onPageChange={handlePageChange}
-      />
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<NavBar />
+			<Searchbar />
+			<CardList allProducts={currentProducts} />
+			<Pagination
+				totalPages={Math.ceil(allProducts.length / itemsPerPage)}
+				currentPage={currentPage}
+				onPageChange={handlePageChange}
+			/>
+		</div>
+	);
 };
 
 export default Home;
--- Client/src/Components/NavBar/NavBar.jsx
+++ Client/src/Components/NavBar/NavBar.jsx
@@ -1,39 +1,39 @@
-import { NavLink } from "react-router-dom";
-import styles from "./NavBar.module.css";
-import glamify from "../../assets/Glamify-logo-negro.png";
-import woman from "../../assets/woman.jpg";
+import { NavLink } from 'react-router-dom';
+import styles from './NavBar.module.css';
+import glamify from '../../assets/Glamify-logo-negro.png';
+import woman from '../../assets/woman.jpg';
 
 const NavBar = () => {
-  return (
-    <div className={styles.NavBarContainer}>
-      {/* <img src={woman} alt="woman" className={styles.imageWoman} /> */}
-      <div className={styles.logoContainer}>
-        <NavLink className={styles.create} to="/create">
-          <img
-            src={glamify}
-            alt="Glamify Logo"
-            className={styles.logo}
-            title="Glamify"
-          />
-        </NavLink>
-      </div>
+	return (
+		<div className={styles.NavBarContainer}>
+			{/* <img src={woman} alt="woman" className={styles.imageWoman} /> */}
+			<div className={styles.logoContainer}>
+				<NavLink className={styles.create} to='/create'>
+					<img
+						src={glamify}
+						alt='Glamify Logo'
+						className={styles.logo}
+						title='Glamify'
+					/>
+				</NavLink>
+			</div>
 
-      <div className={styles.containersecondary}>
-        <NavLink to="/home" className={styles.NavLink} title="HOME">
-          HOME
-        </NavLink>
-        <NavLink to="/hombre" className={styles.NavLink} title="HOMBRE">
-          HOMBRE
-        </NavLink>
-        <NavLink to="/mujer" className={styles.NavLink} title="MUJER">
-          MUJER
-        </NavLink>
-        <NavLink to="/accesorios" className={styles.NavLink} title="ACCESORIOS">
-          ACCESORIOS
-        </NavLink>
-      </div>
-    </div>
-  );
+			<div className={styles.containersecondary}>
+				<NavLink to='/home' className={styles.NavLink} title='HOME'>
+					HOME
+				</NavLink>
+				<NavLink to='/hombre' className={styles.NavLink} title='HOMBRE'>
+					HOMBRE
+				</NavLink>
+				<NavLink to='/mujer' className={styles.NavLink} title='MUJER'>
+					MUJER
+				</NavLink>
+				<NavLink to='/accesorios' className={styles.NavLink} title='ACCESORIOS'>
+					ACCESORIOS
+				</NavLink>
+			</div>
+		</div>
+	);
 };
 
 export default NavBar;
