aa5fe7c995a32676062398706e9a4edb5291e129
nfuenzalidaz <119084817+nfuenzalidaz@users.noreply.github.com>
2023-09-20 23:59:37 -0300

    Update User.js

--- Server/src/models/User.js
+++ Server/src/models/User.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 
 const userModel = (sequelize) => {
     sequelize.define(
-        'user',
+        'User',
         {
             id: {
                 type: DataTypes.UUID,
