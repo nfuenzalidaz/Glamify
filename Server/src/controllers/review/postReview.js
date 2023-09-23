35f6b84c3e874a612861378b1c92f32494d420bc
nfuenzalidaz <119084817+nfuenzalidaz@users.noreply.github.com>
2023-09-22 22:44:14 -0300

    Update postReview.js

--- Server/src/controllers/review/postReview.js
+++ Server/src/controllers/review/postReview.js
@@ -3,7 +3,7 @@ const { Product, User, Review } = require('../../db');
 const createReviewController = async (rating, comment, productId, userId) => {
     const user = await User.findByPk(userId);
     const product = await Product.findByPk(productId);
-    if (!user && !product) {
+    if (!user || !product) {
         throw new Error('User not found or product not found')
     }
 
