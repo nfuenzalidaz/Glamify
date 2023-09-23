df91502a29fb03003ba85ac08c5069533664435c
Jose Enriquez <120431604+JoseEnriquez88@users.noreply.github.com>
2023-09-23 02:41:17 -0300

    Update getSaleByDate.js

--- Server/src/controllers/sale/getSaleByDate.js
+++ Server/src/controllers/sale/getSaleByDate.js
@@ -1,12 +1,12 @@
-const { Sail } = require('../../db');
+const { Sale } = require('../../db');
 const { Op } = require('sequelize');
 
-const getSailByDate = async (date) => {
-  const data = await Sail.findAll({
+const getSaleByDate = async (date) => {
+  const data = await Sale.findAll({
     where: { date: { [Op.eq]: date } },
   });
   if (data.length === 0) throw new Error(`No se encontró ningún registro con la fecha: ${date}.`);
   return [...data];
 };
 
-module.exports = getSailByDate;
+module.exports = getSaleByDate;
