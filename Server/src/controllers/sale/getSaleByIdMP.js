16346a450a7a8a6ea0ad76b3ecafd1c46ddf2532
Jose Enriquez <120431604+JoseEnriquez88@users.noreply.github.com>
2023-09-23 02:41:50 -0300

    Update getSaleByIdMP.js

--- Server/src/controllers/sale/getSaleByIdMP.js
+++ Server/src/controllers/sale/getSaleByIdMP.js
@@ -1,12 +1,12 @@
-const { Sail } = require('../../db');
+const { Sale } = require('../../db');
 const { Op } = require('sequelize');
 
-const getSailByIdMP = async (idMercadoPago) => {
-  const data = await Sail.findAll({
+const getSaleByIdMP = async (idMercadoPago) => {
+  const data = await Sale.findAll({
     where: { id_mercado_pago: { [Op.eq]: idMercadoPago } },
   });
   if (data.length === 0) throw new Error(`No se encontraron ventas con el ID de Mercado Pago: ${idMercadoPago}.`);
   return [...data];
 };
 
-module.exports = getSailByIdMP;
+module.exports = getSaleByIdMP;
