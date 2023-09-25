c7dd13e6555e2ceaf20e219c4b7bec111d6cfe58
nfuenzalidaz <119084817+nfuenzalidaz@users.noreply.github.com>
2023-09-25 15:39:54 -0300

    Validaciones

--- Server/src/helpers/postProductValidation.js
+++ Server/src/helpers/postProductValidation.js
@@ -4,23 +4,23 @@ const imageRegex = /^.+\.(jpeg|jpg|png)$/;
 const imageRegexURL = /(https?:\/\/.*\.(?:png|jpg|jpeg))/i;
 const stockRegex = /^[0-9]+$/;
 
-const postProductValidation = (name, description, image, price, category, stock) => {
+const postProductValidation = (name, description, image, price, stock, category, gender ) => {
 
     // name
     //if (!nameRegex.test(name)) throw new Error(`El nombre ${name} no es válido para crear el producto.`);
-    if (name === "" || name.length === 0) throw new Error(`El campo "nombre" del producto no puede estar vacío.`);
+    //if (name === "" || name.length === 0) throw new Error(`El campo "nombre" del producto no puede estar vacío.`);
     //if (name.length > 50) throw new Error(`El nombre del producto solo puede contener máximo 50 caracteres.`);
 
     // description
-    if (description === '' || description.length > 255) throw new Error('La descripción del producto debe tener entre 1 y 255 caracteres.');
+    //if (description === '' || description.length > 255) throw new Error('La descripción del producto debe tener entre 1 y 255 caracteres.');
 
     //image
-    if (image.length === '') throw new Error('Es necesario que el producto contenga una imagen para poder crearlo.');
+    //if (image.length === '') throw new Error('Es necesario que el producto contenga una imagen para poder crearlo.');
     // if (!imageRegex.test(image) || !imageRegexURL.test(image)) throw new Error('La imagen debe ser una URL válida o tener un formato jpg, jpeg, png');
 
     // price
-    if (!priceRegex.test(price)) throw new Error('El precio debe ser numerico.');
-    if (price === "") throw new Error('El campo "precio" del producto no puede estar vacío.');
+    //if (!priceRegex.test(price)) throw new Error('El precio debe ser numerico.');
+    //if (price === "") throw new Error('El campo "precio" del producto no puede estar vacío.');
 
     // categor
     // if (!category || category.length > 50) throw new Error('La categoría del producto es obligatoria y debe tener menos de 50 caracteres.');
