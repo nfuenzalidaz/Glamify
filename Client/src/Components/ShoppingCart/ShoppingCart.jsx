bb4e493ea4ee3a5acec1c4295ef4c8af0692702b
NFuenzalida <n.fuenzalida.z@gmail.com>
2023-10-02 23:42:05 -0300

    Cambios modelos (userId externo)

--- Client/src/Components/ShoppingCart/ShoppingCart.jsx
+++ Client/src/Components/ShoppingCart/ShoppingCart.jsx
@@ -6,9 +6,8 @@ import {
   clearCart,
 } from '../../Redux/Features/cartSlice';
 import styles from './ShoppingCart.module.css';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
+import { Toaster, toast } from 'react-hot-toast';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import axios from 'axios';
@@ -19,48 +18,71 @@ const ShoppingCart = () => {
   const cart = useSelector((state) => state.cart);
   const { user, isAuthenticated } = useAuth0();
 
+  const notifyUser = () =>
+    toast.error('Debe iniciar sesion para comprar', {
+      position: 'bottom-center',
+    });
+
+  const notifyCart = () =>
+    toast.error('Debe agregar al menos un producto al carrito', {
+      position: 'bottom-center',
+    });
+
   const handlePay = () => {
     event.preventDefault();
 
     if (cartItems.length === 0) {
       return;
     }
-
-    const response = axios
-      .post('/payment/create-order', { user, cart })
-      .then((res) => (window.location.href = res.data.init_point));
-    };
+    if (user && cart) {
+      try {
+        const response = axios
+          .post('/payment/create-order', { user, cart })
+          .then((res) => (window.location.href = res.data.init_point));
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      if (!user) notifyUser();
+      if (!cart) notifyCart();
+    }
+  };
 
   return (
     <div className={styles.items}>
       <div className={styles.flushIcon}>
-      <button className={styles.buttomEmpty}onClick={() => dispatch(clearCart())}><DeleteForeverIcon/></button>
+        <button
+          className={styles.buttomEmpty}
+          onClick={() => dispatch(clearCart())}
+        >
+          <DeleteForeverIcon />
+        </button>
       </div>
       {cartItems.map((item) => (
-        <div className={styles.idKey} key={item.id} >
+        <div className={styles.idKey} key={item.id}>
           <div className={styles.ShoppingCartMain}>
-          <img className={styles.Image} src={item.image} alt={item.name} />
-          <div className={styles.buttonsContainer}>
-            <button
-              className={styles.deleteButtom}
-              onClick={() => dispatch(removeItemFromCart(item.id))}
-            >
-              <DeleteOutlineIcon />
-            </button>
-            <button
-              className={styles.decre}
-              onClick={() => dispatch(decreaseItemQuantity(item.id))}
-            >
-              -
-            </button>
-            <span className={styles.quantity}>{item.quantity}</span>
-            <button
-              className={styles.incre}
-              onClick={() => dispatch(increaseItemQuantity(item.id))}
-            >
-              +
-            </button>
-          </div>
+            <img className={styles.Image} src={item.image} alt={item.name} />
+            <div className={styles.buttonsContainer}>
+              <button
+                className={styles.deleteButtom}
+                onClick={() => dispatch(removeItemFromCart(item.id))}
+              >
+                <DeleteOutlineIcon />
+              </button>
+              <button
+                className={styles.decre}
+                onClick={() => dispatch(decreaseItemQuantity(item.id))}
+              >
+                -
+              </button>
+              <span className={styles.quantity}>{item.quantity}</span>
+              <button
+                className={styles.incre}
+                onClick={() => dispatch(increaseItemQuantity(item.id))}
+              >
+                +
+              </button>
+            </div>
           </div>
           <div className={styles.textContainer}>
             <h3>{item.name}</h3>
@@ -71,8 +93,25 @@ const ShoppingCart = () => {
       ))}
       <div className={styles.total}>
         <h2>TOTAL ${cart.totalPrice}</h2>
-      <button className={styles.buttonPay}onClick={handlePay} disabled={cartItems.length === 0} >PAGAR</button>
+        <button
+          className={styles.buttonPay}
+          onClick={handlePay}
+          disabled={cartItems.length === 0}
+        >
+          PAGAR
+        </button>
       </div>
+      <Toaster
+        toastOptions={{
+          className: '',
+          style: {
+            border: '2px solid #000000',
+            padding: '10px',
+            color: '#ffffff',
+            background: '#000000',
+          },
+        }}
+      />
     </div>
   );
 };
