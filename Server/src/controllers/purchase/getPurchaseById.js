29daf3fcd90d5fa516f81993e708bdebd19413e0
NFuenzalida <n.fuenzalida.z@gmail.com>
2023-10-05 09:53:28 -0300

    Modificacion endpoint purchase

--- Server/src/controllers/purchase/getPurchaseById.js
+++ Server/src/controllers/purchase/getPurchaseById.js
@@ -1,10 +1,16 @@
-const { Purchase_Detail } = require('../../db');
+const { Purchase_Detail, Product } = require('../../db');
 
 const getPurchaseByIdController = async (PurchaseId) => {
   try {
     const purchase = await Purchase_Detail.findAll({
       attributes: ["ProductId", "quantity"],
       where: { PurchaseId },
+      include: [
+        {
+          model: Product,
+          attributes: ["name"],
+        }
+      ],
     });
     return purchase;
   } catch (error) {
