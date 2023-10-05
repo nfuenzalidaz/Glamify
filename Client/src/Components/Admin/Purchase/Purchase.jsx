5feecf30286629184cb7dad7e8fdec1fed8e2c0a
NFuenzalida <n.fuenzalida.z@gmail.com>
2023-10-05 15:57:57 -0300

    Componente purchase

--- Client/src/Components/Admin/Purchase/Purchase.jsx
+++ Client/src/Components/Admin/Purchase/Purchase.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
-import AdminNavBar from '../AdminNavBar/AdminNavBar';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchAllPurchases } from "../../../Redux/Features/purchaseSlice";
+import AdminNavBar from "../AdminNavBar/AdminNavBar";
+
 const Purchase = () => {
+  const dispatch = useDispatch();
+  const allPurchases = useSelector((state) => state.purchases.allPurchases);
+  const loading = useSelector((state) => state.purchases.loading);
+
+  useEffect(() => {
+    dispatch(fetchAllPurchases());
+  }, [dispatch]);
+
   return (
     <div>
       <AdminNavBar />
-      <h2>Historial de Ventas</h2>
+      <h2>HISTORIAL DE VENTAS</h2>
+      {loading ? (
+        <p>CARGANDO...</p>
+      ) : (
+        <ul>
+          {allPurchases.map((purchase) => (
+            <li key={purchase.id}>
+              <p>Usuario: {purchase.userId}</p>
+              <p>Fecha: {purchase.createdAt}</p>
+              <p>Total: ${purchase.total}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
 
-export default Purchase;
+export default Purchase; 
